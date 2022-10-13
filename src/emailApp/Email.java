bee613eff78e4d61b26e7559ce936797cd5466be
Louisss-Lam <louislam617@gmail.com>
2022-10-13 16:36:21 +0100

    testing

---
+++ src/emailApp/Email.java
@@ -0,0 +1,26 @@
+package emailApp;
+
+public class Email {
+	private String firstname;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailBoxCapacity;
+	private String alternateEmail;
+	
+	// Constructor to receive the first name and last name
+	public Email(String firstName, String lastName) {
+		this.firstname = firstName;
+		this.lastName = lastName;
+		System.out.println("EMAIL CREATED: " + this.firstname + " " + this.lastName);
+		
+	}
+	
+	// Ask got department
+	
+	// Generate a random password
+	
+	// Set the alternate email
+	
+	// Change the password
+}
